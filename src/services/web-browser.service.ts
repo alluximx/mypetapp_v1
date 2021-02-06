bbcf01c54b471997df22a25c35a12f67442a4ae8
Gustavo Reyes <gustavo@alluxi.com>
2021-02-06 12:56:57 -0600

    Fixes on kitten lib

--- src/services/web-browser.service.ts
+++ src/services/web-browser.service.ts
@@ -1,11 +1,10 @@
-import { Linkin, Platform } from 'react-native';
+import { Linking, Platform } from 'react-native';
 
 export class WebBrowserService {
 
   static openBrowserAsync = (url: string): Promise<any> => {
     return WebBrowserService.openUrl(url);
   };
-
 
   private static openUrl = (url: string): Promise<any> => {
     return Linking.openURL(url);
