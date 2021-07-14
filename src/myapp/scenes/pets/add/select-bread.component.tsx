7b14b37c89543be29dfa10599f424d75a5d410f5
Erik <erikvega@alluxi.com>
2021-07-14 14:11:40 -0500

    delete components with breads

--- src/myapp/scenes/pets/add/select-bread.component.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ContentView from '../../../layouts/pets/add/select-bread';
-
-export const SelectBreadScreen = ({navigation}) : React.ReactElement => {
-    return <ContentView navigation={navigation} />;
-} 
