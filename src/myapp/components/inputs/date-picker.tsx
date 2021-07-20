fc32185c87493da0224f36ef877b3c295b20a253
Erik <erikvega@alluxi.com>
2021-07-20 18:09:39 -0500

    change datePicker to add a custom label

--- src/myapp/components/inputs/date-picker.tsx
+++ src/myapp/components/inputs/date-picker.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {Animated, StyleSheet, View, Text} from 'react-native';
 import {Datepicker, NativeDateService} from '@ui-kitten/components';
 import moment from 'moment';
 // My Components.
 import {DropDownIcon} from '../icons';
+// Global Styles.
+import globalColors from '../../styles/colors';
+import globalVars from '../../styles/vars';
 
 const i18n = {
   dayNames: {
@@ -64,33 +67,80 @@ const minDate = new Date(
 );
 
 const DatepickerInput = (props) => {
+  const focusAnim = useRef<Animated.Value>(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(focusAnim, {
+      toValue: props.currentValue !== '' ? 0 : 1,
+      duration: 200,
+      useNativeDriver: false,
+    }).start();
+  }, [props.currentValue]);
+
   return (
-    <Datepicker
-      style={styles.container}
-      size="large"
-      placeholder="DD/MM/AAAA"
-      dateService={localeDateService}
-      date={props.currentValue !== '' ? new Date(props.currentValue) : now}
-      onSelect={(date) => {
-        const formattedDate = moment(date).format('YYYY-MM-DD');
-        props.onSelect(formattedDate);
-      }}
-      accessoryRight={CalendarIcon}
-      min={minDate}
-      max={now}
-    />
+    <View>
+      <Animated.Text
+        style={[
+          styles.inputLabel,
+          // Label animations
+          {
+            top: focusAnim.interpolate({
+              inputRange: [0, 1],
+              outputRange: [6, 16],
+            }),
+            fontSize: focusAnim.interpolate({
+              inputRange: [0, 1],
+              outputRange: [14, 16],
+            }),
+            color: focusAnim.interpolate({
+              inputRange: [0, 1],
+              outputRange: [globalColors.darkGray, globalColors.lightGray],
+            }),
+          },
+        ]}>
+        {props.placeholder}
+      </Animated.Text>
+      <Datepicker
+        controlStyle={styles.container}
+        size="large"
+        placeholder={(props) => (
+          <Text {...props} style={[props.style, styles.placeholder]}>
+            props.placeholder
+          </Text>
+        )}
+        dateService={localeDateService}
+        date={props.currentValue !== '' ? new Date(props.currentValue) : null}
+        onSelect={(date) => {
+          const formattedDate = moment(date).format('YYYY-MM-DD');
+          props.onSelect(formattedDate);
+        }}
+        accessoryRight={CalendarIcon}
+        min={minDate}
+        max={now}
+      />
+    </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
-    marginTop: -10,
-  },
-  datepicker: {
-    marginVertical: -2,
+    height: 56,
+    padding: 15,
+    paddingTop: 25,
   },
   arrowIcon: {
-    top: 0,
+    top: -7,
     right: 0,
+  },
+  inputLabel: {
+    color: globalColors.darkGray,
+    fontFamily: globalVars.fontRegular,
+    fontSize: 14,
+    left: 16,
+    position: 'absolute',
+    zIndex: 5,
+  },
+  placeholder: {
+    color: 'transparent',
   },
 });
 export default DatepickerInput;
