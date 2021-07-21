a6582eead4121d2f836712359cdd105b72daa2d6
Aldair-santibanez <aldair@alluxi.com>
2021-07-21 09:36:36 -0500

    completed medical visit service

--- src/myapp/layouts/visits/inf/index.tsx
+++ src/myapp/layouts/visits/inf/index.tsx
@@ -4,47 +4,26 @@ import {
   useStyleSheet,
   Text,
   List,
+  Spinner,
 } from '@ui-kitten/components';
 import React, {useLayoutEffect} from 'react';
-import {Image, Dimensions} from 'react-native';
+import {Image, Dimensions, View} from 'react-native';
 import DefaultLayout from '../../../components/layouts/default-layout';
 import globalColors from '../../../styles/colors';
 import AddButton from '../../../components/buttons/add-button';
 import GenericCard from '../../../components/cards/generic-card';
+import useVisitsInformation from '../../../hooks/visits/useVisitsInformation';
+import {useEffect} from 'react';
 export default ({navigation, route}): React.ReactElement => {
+  const {id, breed, name, pet_age, sex} = route.params.pet;
+  const data = useVisitsInformation();
+  const [visits, setVisits] = React.useState([]);
+  useEffect(() => {
+    if (data.data) {
+      setVisits(data.data.data);
+    }
+  }, [data.data]);
   const styles = useStyleSheet(themedStyles);
-  const data = [
-    {
-      date: new Date(),
-      title: 'Vómitos y fiebre',
-      content:
-        'El veterinario dijo que era una reacción normal por la vacuna que le pussieron.',
-      buttonText: 'Editar',
-      buttonAlign: 'right',
-      images: ['../assets/dog-visit.png', '../assets/dog-visit.png'],
-      styleCard: {},
-    },
-    {
-      date: new Date(),
-      title: 'Vómitos y fiebre',
-      content:
-        'El veterinario dijo que era una reacción normal por la vacuna que le pussieron.',
-      buttonText: 'Editar',
-      buttonAlign: 'right',
-      images: ['../assets/dog-visit.png', '../assets/dog-visit.png'],
-      styleCard: {},
-    },
-    {
-      date: null,
-      title: 'Patita lastimada',
-      content:
-        'Hay que llevar a Valerio a una terapia especial para que se pueda recuperar totalmemte y no siga cojeando. El veterinario va a pasar el contacto de una clínica especializada.',
-      buttonText: 'Editar',
-      buttonAlign: 'right',
-      images: ['../assets/dog-visit.png', '../assets/dog-visit.png'],
-      styleCard: {},
-    },
-  ];
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => (
@@ -61,48 +40,60 @@ export default ({navigation, route}): React.ReactElement => {
     });
   }, [navigation]);
   const renderServiceItem = (service) => {
-    return <GenericCard data={service.item} />;
+    const auxData = {
+      date:
+        service.item.visit_date == null
+          ? null
+          : new Date(service.item.visit_date),
+      title: service.item.title,
+      content: service.item.details,
+      buttonText: 'Editar',
+      buttonAlign: 'right',
+      images: [],
+      styleCard: {},
+    };
+    return <GenericCard data={auxData} />;
   };
-  if (data.length > 0) {
-    return (
-      <DefaultLayout style={[styles.container, {color: 'black'}]}>
-        <Layout
-          style={[
-            styles.formContainer,
-            {backgroundColor: globalColors.backgroundDefault},
-          ]}>
-          <Text style={styles.title}>Visitas Veterinario</Text>
-          <List
-            style={styles.servicesContainer}
-            data={data}
-            renderItem={renderServiceItem}
-          />
-        </Layout>
-      </DefaultLayout>
-    );
-  } else {
-    return (
-      <DefaultLayout style={[styles.container, {color: 'black'}]}>
-        <Layout
-          style={[
-            styles.formContainer,
-            {backgroundColor: globalColors.backgroundDefault},
-          ]}>
-          <Image
-            style={styles.dogImage}
-            source={require('../assets/dog-visit.png')}
-          />
-          <Text style={styles.h1}>Visitas Veterinario.</Text>
-          <Text style={styles.textLabel} category="label">
-            Aún no has agregado visitas al
-          </Text>
-          <Text style={styles.textLabel} category="label">
-            veterinario para tu mascota
-          </Text>
-        </Layout>
-      </DefaultLayout>
-    );
-  }
+  return data.isLoading ? (
+    <View style={styles.viewContainer}>
+      <Spinner size="large" status="success" />
+    </View>
+  ) : visits.length > 0 ? (
+    <DefaultLayout style={[styles.container, {color: 'black'}]}>
+      <Layout
+        style={[
+          styles.formContainer,
+          {backgroundColor: globalColors.backgroundDefault},
+        ]}>
+        <Text style={styles.title}>Visitas Veterinario</Text>
+        <List
+          style={styles.servicesContainer}
+          data={visits}
+          renderItem={renderServiceItem}
+        />
+      </Layout>
+    </DefaultLayout>
+  ) : (
+    <DefaultLayout style={[styles.container, {color: 'black'}]}>
+      <Layout
+        style={[
+          styles.formContainer,
+          {backgroundColor: globalColors.backgroundDefault},
+        ]}>
+        <Image
+          style={styles.dogImage}
+          source={require('../assets/dog-visit.png')}
+        />
+        <Text style={styles.h1}>Visitas Veterinario.</Text>
+        <Text style={styles.textLabel} category="label">
+          Aún no has agregado visitas al
+        </Text>
+        <Text style={styles.textLabel} category="label">
+          veterinario para tu mascota
+        </Text>
+      </Layout>
+    </DefaultLayout>
+  );
 };
 const {width, height} = Dimensions.get('window');
 const themedStyles = StyleService.create({
@@ -189,5 +180,10 @@ const themedStyles = StyleService.create({
     paddingBottom: 8,
     backgroundColor: 'transparent',
     width: width,
+  },
+  viewContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
