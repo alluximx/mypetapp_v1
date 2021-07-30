52e6dc7fe37b1e0505c6b8e7075ee720497dcbc5
Erik <erikvega@alluxi.com>
2021-07-30 18:07:57 -0500

    Vaccine index API Integration

--- src/myapp/hooks/vaccines/useGetVaccineIndex.ts
+++ src/myapp/hooks/vaccines/useGetVaccineIndex.ts
@@ -2,7 +2,7 @@ import {useQuery} from 'react-query';
 import api from '../../services/app-services';
 
 const useGetVaccineIndex = (id) => {
-  return useQuery('use-vaccine-index', () =>
+  return useQuery(['use-vaccine-index', id], () =>
     api.get('api/v1/vaccines-history?user_pet=' + id, true),
   );
 };
--- src/myapp/layouts/vaccines/vaccine-index/index.tsx
+++ src/myapp/layouts/vaccines/vaccine-index/index.tsx
@@ -14,33 +14,81 @@ import useGetVaccineIndex from '../../../hooks/vaccines/useGetVaccineIndex';
 export default ({navigation, route}): React.ReactElement => {
   const {id, breed, name, pet_age, sex} = route.params.pet;
 
-  /* const vaccines = null;
-  const vaccines2 = null; */
-
   const [vaccines, setVaccines] = useState([]);
   const vaccinesQuery = useGetVaccineIndex(id);
-  //console.log('data');
-  //console.log(vaccinesQuery.data);
 
   useEffect(() => {
     if (vaccinesQuery.data) {
-      setVaccines(vaccinesQuery.data.data);
+      function sortByDate(a, b) {
+        var Item1 = a.vaccine_date;
+        var Item2 = b.vaccine_date;
+        if (Item1 < Item2) {
+          return 1;
+        }
+        if (Item1 > Item2) {
+          return -1;
+        }
+        return 0;
+      }
+      vaccinesQuery.data.data.sort(sortByDate);
+      const groupVaccines = vaccinesQuery.data.data.reduce((acc, obj) => {
+        const id = obj.vaccine_registered.id;
+        const formatedVaccine = {id: obj.id, date: obj.vaccine_date};
+        if (!acc[id]) {
+          acc[id] = {
+            id_vaccine: id,
+            id_record: obj.id,
+            name: obj.vaccine_registered.vaccine_name,
+            reminder: obj.reminder,
+            is_unique: obj.vaccine_registered.is_unique,
+            next_vaccine_date: obj.next_vaccine_date,
+            vaccine_date: obj.vaccine_date,
+            vaccines: [formatedVaccine],
+          };
+        } else {
+          acc[id].vaccines.push(formatedVaccine);
+        }
+        return acc;
+      }, {});
+
+      const formatedArray: any = [];
+      const claves = Object.keys(groupVaccines);
+
+      claves.forEach((element) => {
+        formatedArray.push(groupVaccines[element]);
+      });
+
+      setVaccines(formatedArray);
     }
   }, [vaccinesQuery.data]);
 
-  console.log('VaccinesData');
-  console.log(vaccines);
+  function customSort(a, b) {
+    var Item1 = a.name;
+    var Item2 = b.name;
+    if (Item1 > Item2) {
+      return 1;
+    }
+    if (Item1 < Item2) {
+      return -1;
+    }
+    return 0;
+  }
+
+  vaccines.sort(customSort);
 
   const renderServiceItem = (service) => {
-    console.log('Servicio');
-    console.log(service);
+    const is_unique = service.item.is_unique;
+    const next_vaccine_date = new Date(service.item.next_vaccine_date);
+    const notification = new Date(service.item.reminder);
+    const substrac = next_vaccine_date.getTime() - notification.getTime();
+    const notificationDays = Math.round(substrac / (1000 * 60 * 60 * 24) + 1);
     const auxData = {
-      //name: service.item.id,
-      name: 'ParaInfluenza',
-      validity: service.item.vaccine_date,
-      notification: '3 semana antes',
-      status: 'Activa',
-      vaccineDates: service.item.vaccineDates,
+      name: service.item.name,
+      validity: is_unique ? 'Unica' : service.item.vaccine_date,
+      notification: !is_unique ? notificationDays : null,
+      status:
+        is_unique || next_vaccine_date > new Date() ? 'Activa' : 'Vencida',
+      vaccineDates: service.item.vaccines,
     };
     return <VaccineCard data={auxData} />;
   };
@@ -61,7 +109,7 @@ export default ({navigation, route}): React.ReactElement => {
     });
   }, [navigation]);
 
-  return vaccines !== null ? (
+  return vaccines.length > 0 ? (
     <DefaultLayout>
       <Text style={styles.titleh1}>Vacunas</Text>
       <List
