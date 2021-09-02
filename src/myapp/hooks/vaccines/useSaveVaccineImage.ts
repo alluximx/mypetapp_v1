ae3d1db2aef0da4de4553a923d55bc74349330cb
ALLUXI <alluxi@mac.com>
2021-09-02 07:42:33 -0500

    Camera image path corrections

--- src/myapp/hooks/vaccines/useSaveVaccineImage.ts
+++ src/myapp/hooks/vaccines/useSaveVaccineImage.ts
@@ -1,15 +1,20 @@
+import {Platform} from 'react-native';
 import {useMutation} from 'react-query';
 import RNFetchBlob from 'rn-fetch-blob';
 import api from '../../services/app-services';
 
 const postVaccineImage = (data) => {
+  const realPath =
+    Platform.OS === 'ios'
+      ? decodeURIComponent(data.file.uri.replace('file://', ''))
+      : data.file.uri;
   const newData = [
     {name: 'vaccine', data: data.vaccine},
     {
       name: 'file',
       filename: data.file.fileName,
       type: data.file.type,
-      data: RNFetchBlob.wrap(data.file.uri),
+      data: RNFetchBlob.wrap(realPath),
     },
   ];
   return api.post('api/v1/vaccine-images/', newData, true, true);
--- src/myapp/hooks/vaccines/useUpdateVaccineImage.ts
+++ src/myapp/hooks/vaccines/useUpdateVaccineImage.ts
@@ -1,15 +1,20 @@
+import {Platform} from 'react-native';
 import {useMutation} from 'react-query';
 import RNFetchBlob from 'rn-fetch-blob';
 import api from '../../services/app-services';
 
 const putVaccineImage = (data) => {
+  const realPath =
+    Platform.OS === 'ios'
+      ? decodeURIComponent(data.file.uri.replace('file://', ''))
+      : data.file.uri;
   const newData = [
     {name: 'vaccine', data: data.vaccine},
     {
       name: 'file',
       filename: data.file.fileName,
       type: data.file.type,
-      data: RNFetchBlob.wrap(data.file.uri),
+      data: RNFetchBlob.wrap(realPath),
     },
   ];
 
