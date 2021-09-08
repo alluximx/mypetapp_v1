db27d6751760fca6a6e85ed3fb7db7ccc3e4da47
Oscar Alejandro López Ramos <oscar@alluxi.com>
2021-09-08 14:54:51 -0500

    Fix for pet image input on iOS

--- src/myapp/hooks/pets/useSavePetImage.ts
+++ src/myapp/hooks/pets/useSavePetImage.ts
@@ -1,15 +1,20 @@
+import {Platform} from 'react-native';
 import {useMutation} from 'react-query';
 import RNFetchBlob from 'rn-fetch-blob';
 import api from '../../services/app-services';
 
 const postPetImage = (data) => {
+  const realPath =
+    Platform.OS === 'ios'
+      ? decodeURIComponent(data.file.uri.replace('file://', ''))
+      : data.file.uri;
   const newData = [
     {name: 'pet_image', data: data.pet_image},
     {
       name: 'file',
       filename: data.file.fileName,
       type: data.file.type,
-      data: RNFetchBlob.wrap(data.file.uri),
+      data: RNFetchBlob.wrap(realPath),
     },
   ];
   return api.post('api/v1/pets_images/', newData, true, true);
