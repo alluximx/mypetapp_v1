cbd1e95e41d2df19489c8be6723ec6dc77898794
Oscar Alejandro López Ramos <oscar@alluxi.com>
2021-09-09 10:45:56 -0500

    Test

--- src/myapp/layouts/products/index.tsx
+++ src/myapp/layouts/products/index.tsx
@@ -3,5 +3,6 @@ import React, {useLayoutEffect, useEffect, useState} from 'react';
 import DefaultText from '../../components/texts/default-text';
 
 export default (): React.ReactElement => {
+  console.log('hola mundo');
   return <DefaultText children="Hola Mundo" />;
 };
