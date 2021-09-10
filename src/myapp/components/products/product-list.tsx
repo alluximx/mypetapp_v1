467a8c6207a41415a76b43c881fc4b36c9cdcc90
Oscar Alejandro López Ramos <oscar@alluxi.com>
2021-09-10 06:55:52 -0500

    Product index by category

--- src/myapp/components/products/product-list.tsx
+++ src/myapp/components/products/product-list.tsx
@@ -1,26 +1,43 @@
 import {List} from '@ui-kitten/components';
 import React from 'react';
 import {StyleSheet} from 'react-native';
+// Global Styles.
+import globalColors from '../../styles/colors';
 // Hooks.
 import useProductsList from '../../hooks/products/useProductsList';
 // My Components.
 import CustomSpinner from '../custom-spinner';
+import ProductCard from './product-card';
+// Types.
+import {ProductListProps} from '../../types/components/products';
 
-const ProductList = (props): React.ReactElement => {
-  const {data, isLoading} = useProductsList(props.categoryId);
+const ProductList = (props: ProductListProps): React.ReactElement => {
+  const {data, isLoading} = useProductsList(
+    props.categoryId ?? '',
+    props.brandId ?? '',
+  );
 
   return isLoading ? (
     <CustomSpinner />
   ) : (
-    // <List data={data ? data.data : []} renderItem={} />
-    <CustomSpinner />
+    <List
+      data={data ? data.data : []}
+      renderItem={({item}) => (
+        <ProductCard
+          brand={item.brand.name}
+          name={item.name}
+          cover_image={item.cover_image}
+        />
+      )}
+      style={styles.container}
+    />
   );
 };
 
 const styles = StyleSheet.create({
-  loadingContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+  container: {
+    backgroundColor: globalColors.backgroundDefault,
+    marginTop: 16,
   },
 });
 
---
+++ src/myapp/types/components/products.ts
@@ -0,0 +1,10 @@
+export interface ProductListProps {
+  categoryId?: string;
+  brandId?: string;
+}
+
+export interface ProductCardProps {
+  cover_image?: string;
+  brand: string;
+  name: string;
+}
