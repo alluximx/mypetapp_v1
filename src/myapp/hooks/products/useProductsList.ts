9d410d945d8e11093c3b17cb699ba7ad687a9eab
Oscar Alejandro López Ramos <oscar@alluxi.com>
2021-09-15 00:24:51 -0500

    Search by keyword

--- src/myapp/hooks/products/useProductsList.ts
+++ src/myapp/hooks/products/useProductsList.ts
@@ -4,7 +4,7 @@ import api from '../../services/app-services';
 const useProductsList = (categoryId = '', name = '', brandId = '') =>
   useQuery(['products', categoryId, name, brandId], () =>
     api.get(
-      `api/v1/products/?brand=${brandId}&category=${categoryId}&is_active=true&name=${name}`,
+      `api/v1/products/?brand=${brandId}&category=${categoryId}&is_active=true&name__icontains=${name}`,
       true,
     ),
   );
