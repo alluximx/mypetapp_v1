4517bd4fb7b31ddb950a313698f245ec84957cfc
Oscar Alejandro López Ramos <oscar@alluxi.com>
2021-09-17 03:03:17 -0500

    Add price to product list

--- src/myapp/components/products/product-card.tsx
+++ src/myapp/components/products/product-card.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, TouchableOpacity} from 'react-native';
+import {StyleSheet} from 'react-native';
 // Global Styles.
 import globalColors from '../../styles/colors';
 // My Components.
@@ -8,16 +8,20 @@ import GenericCard from '../cards/generic-card';
 import {ProductCardProps} from '../../types/components/products';
 
 const ProductCard = (props: ProductCardProps): React.ReactElement => {
+  const {brand, cover_image, name, onPress, range_prices} = props;
+
+  const price = (range_prices.price__max + range_prices.price__min) / 2;
+
   return (
     <GenericCard
       data={{
-        buttonText: '$200.00',
+        buttonText: `$${price.toFixed(2)}`,
         buttonAlign: 'right',
-        content: props.brand,
-        coverImage: props.cover_image,
-        title: props.name,
+        content: brand,
+        coverImage: cover_image,
+        title: name,
       }}
-      onClick={props.onPress}
+      onClick={onPress}
       buttonStyle={styles.price}
       contentTextStyle={styles.subtitle}
     />
