d64c772925f62486fd302523c88780240da73a55
Oscar Alejandro López Ramos <oscar@alluxi.com>
2021-09-22 16:09:54 -0500

    WIP: Add bubble icon in cart logo

--- src/myapp/layouts/products/detail/index.tsx
+++ src/myapp/layouts/products/detail/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useRef} from 'react';
+import React, {useEffect, useState, useRef, useContext} from 'react';
 import {
   Layout,
   Spinner,
@@ -7,6 +7,8 @@ import {
 } from '@ui-kitten/components';
 import {Dimensions, Image, View, ScrollView} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
+// Context
+import {AuthContext} from '../../../context/AuthContext';
 // Global Styles
 import globalColors from '../../../styles/colors';
 import globalStyles from '../../../styles/style';
@@ -24,9 +26,11 @@ import TitleHeader from '../../../components/texts/title-header';
 import {BaseModel} from '../../../types/models';
 
 const INITIAL_INDEX = 0;
+
 export default ({route}): React.ReactElement => {
+  const authContext = useContext(AuthContext);
   const dataVariants = useVariants(route.params.id);
-  const saveProductCart = useSaveProductCart();
+  const saveProductCart = useSaveProductCart(authContext.userId);
   const styles = useStyleSheet(themedStyles);
   const [presentationValue, setPresentationValue] = useState('1');
   const [amountValue, setAmountValue] = useState('1');
@@ -86,14 +90,7 @@ export default ({route}): React.ReactElement => {
 
   const actionButton = () => {
     const data = {item: presentationValue, quantity: parseInt(amountValue, 10)};
-    saveProductCart.mutate(data, {
-      onSuccess: () => {
-        // Action
-      },
-      onError: () => {
-        // Action
-      },
-    });
+    saveProductCart.mutate(data);
   };
 
   const carouselRef = useRef(null);
