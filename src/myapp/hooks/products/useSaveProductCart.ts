a73d140e22861b6d4f43819b68816d379dc5bfdd
Oscar Alejandro López Ramos <oscar@alluxi.com>
2021-09-22 16:09:54 -0500

    WIP: Add bubble icon in cart logo

--- src/myapp/hooks/products/useSaveProductCart.ts
+++ src/myapp/hooks/products/useSaveProductCart.ts
@@ -1,12 +1,21 @@
-import {useMutation} from 'react-query';
+import {useNavigation} from '@react-navigation/native';
+import {QueryClient, useMutation} from 'react-query';
 import api from '../../services/app-services';
 
 const postProductCar = (data) => {
   return api.post('api/v1/shopping-cart/', data, true);
 };
 
-const useSaveProductCart = () => {
-  return useMutation((data: any) => postProductCar(data));
+const useSaveProductCart = (userId: number) => {
+  const queryClient = new QueryClient();
+  const navigation = useNavigation();
+  return useMutation((data: any) => postProductCar(data), {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['shopping-cart', userId]);
+      queryClient.refetchQueries(['shopping-cart', userId]);
+      navigation.navigate('ProductList');
+    },
+  });
 };
 
 export default useSaveProductCart;
