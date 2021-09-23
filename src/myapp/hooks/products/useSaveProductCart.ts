a39eae6aaa47f6b4bb727556953a14eb3ff1e733
Oscar Alejandro López Ramos <oscar@alluxi.com>
2021-09-23 03:06:28 -0500

    Update cart icon when adding product to cart

--- src/myapp/hooks/products/useSaveProductCart.ts
+++ src/myapp/hooks/products/useSaveProductCart.ts
@@ -1,18 +1,16 @@
 import {useNavigation} from '@react-navigation/native';
-import {QueryClient, useMutation} from 'react-query';
+import {useMutation, useQueryClient} from 'react-query';
 import api from '../../services/app-services';
 
-const postProductCar = (data) => {
-  return api.post('api/v1/shopping-cart/', data, true);
-};
+const postProductCar = (data: any) =>
+  api.post('api/v1/shopping-cart/', data, true);
 
-const useSaveProductCart = (userId: number) => {
-  const queryClient = new QueryClient();
+const useSaveProductCart = () => {
+  const queryClient = useQueryClient();
   const navigation = useNavigation();
   return useMutation((data: any) => postProductCar(data), {
     onSuccess: () => {
-      queryClient.invalidateQueries(['shopping-cart', userId]);
-      queryClient.refetchQueries(['shopping-cart', userId]);
+      queryClient.invalidateQueries('shopping-cart');
       navigation.navigate('ProductList');
     },
   });
