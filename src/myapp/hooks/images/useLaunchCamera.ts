c695f6838ad3e9a323d6c637ec8e7f4d8d329d5c
ErikAlluxi <erikvega@alluxi.com>
2021-09-28 18:04:41 -0500

    add gallery to camera component

--- src/myapp/hooks/images/useLaunchCamera.ts
+++ src/myapp/hooks/images/useLaunchCamera.ts
@@ -1,10 +1,26 @@
 import {useCallback} from 'react';
-import {ImageSourcePropType} from 'react-native';
+import {
+  Alert,
+  Image,
+  ImageSourcePropType,
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {
   CameraOptions,
+  launchImageLibrary,
   ImagePickerResponse,
   launchCamera,
 } from 'react-native-image-picker';
+
+const galleryOptions: CameraOptions = {
+  mediaType: 'photo',
+  maxWidth: 500,
+  maxHeight: 500,
+  quality: 0.5,
+};
 
 const options: CameraOptions = {
   mediaType: 'photo',
@@ -15,11 +31,42 @@ const options: CameraOptions = {
 
 const useLaunchCamera = (setImage: (image: ImageSourcePropType) => void) => {
   const onPress = useCallback(() => {
-    launchCamera(options, (response: ImagePickerResponse) => {
-      if (!response.didCancel && !response.errorCode) {
-        setImage(response.assets[0]);
-      }
-    });
+    Alert.alert(
+      'Editar imagen',
+      'Selecciona una opción...',
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+        {
+          text: 'Seleccionar desde mi galería',
+          style: 'default',
+          onPress: () =>
+            launchImageLibrary(
+              galleryOptions,
+              (response: ImagePickerResponse) => {
+                if (!response.didCancel && !response.errorCode) {
+                  setImage(response.assets[0]);
+                }
+              },
+            ),
+        },
+        {
+          text: 'Tomar una nueva foto',
+          style: 'default',
+          onPress: () =>
+            launchCamera(options, (response: ImagePickerResponse) => {
+              if (!response.didCancel && !response.errorCode) {
+                setImage(response.assets[0]);
+              }
+            }),
+        },
+      ],
+      {
+        cancelable: true,
+      },
+    );
   }, []);
 
   const onDelete = () => {
