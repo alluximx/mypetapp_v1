f2c6512304c35d1dc3e518e2190a804efbf3d771
Oscar Alejandro López Ramos <oscar@alluxi.com>
2021-09-28 05:10:27 -0500

    Navigate button refactor bugfix icon not centering

--- src/myapp/components/buttons/navigate-button.tsx
+++ src/myapp/components/buttons/navigate-button.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import {StyleService} from '@ui-kitten/components';
-import {TouchableOpacity, Dimensions, View} from 'react-native';
+import {TouchableOpacity, Dimensions, View, StyleSheet} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 // Global Styles
 import globalColors from '../../styles/colors';
@@ -15,76 +14,64 @@ import {NavigateButtonProps} from '../../types/components/buttons';
 const NavigateButton = (props: NavigateButtonProps): React.ReactElement => {
   const navigation = useNavigation();
   const navigateToScreen = () => {
-    const params = {setValue: props.setValue};
     if (props.data) {
       props.destination &&
-        navigation.navigate(props.destination, {...params, data: props.data});
+        navigation.navigate(props.destination, {data: props.data});
     } else {
-      props.destination && navigation.navigate(props.destination, params);
+      props.destination && navigation.navigate(props.destination);
     }
   };
   return (
     <TouchableOpacity style={styles.container} onPress={navigateToScreen}>
-      <View>
-        {props.title ? (
-          <View style={styles.titleContent}>
-            <TitleHeader style={styles.title}>{props.title}</TitleHeader>
-          </View>
-        ) : (
-          <DefaultText>{}</DefaultText>
-        )}
-
-        <View style={styles.content}>
+      {props.title && props.subtitle ? (
+        <View style={styles.containerPadding}>
+          <TitleHeader style={styles.title}>{props.title}</TitleHeader>
           <DefaultText style={styles.subtitle}>{props.subtitle} </DefaultText>
         </View>
-      </View>
-      <View style={styles.containerIcon}>
-        <DefaultText>{}</DefaultText>
-        <View style={styles.arrowIcon}>
-          <DropRightIcon style={{top: -2}} />
+      ) : (
+        <View style={styles.containerPlaceholder}>
+          <DefaultText style={styles.placeholder}>
+            {props.placeholder}
+          </DefaultText>
         </View>
-      </View>
+      )}
+      <DropRightIcon style={styles.iconStyles} />
     </TouchableOpacity>
   );
 };
 
-const {width} = Dimensions.get('window');
-const styles = StyleService.create({
+const styles = StyleSheet.create({
   container: {
-    width: width - globalVars.outsidePadding - 22,
     borderRadius: 18,
     backgroundColor: globalColors.white,
     marginBottom: 14,
     flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   title: {
-    marginLeft: 14,
     fontSize: 16,
+    marginBottom: 0,
   },
   subtitle: {
-    marginLeft: 14,
     fontSize: 14,
   },
-  containerIcon: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  placeholder: {
+    color: globalColors.darkerGray,
   },
-  arrowIcon: {
-    alignContent: 'flex-end',
-    justifyContent: 'center',
+  iconStyles: {
+    height: 40,
+    width: 40,
+    top: 0,
+    right: 8,
   },
-
-  titleContent: {
-    marginTop: 15,
+  containerPadding: {
+    paddingVertical: 16,
+    paddingHorizontal: 16,
   },
-  emptyTitle: {
-    marginTop: 0,
-  },
-  content: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 15,
+  containerPlaceholder: {
+    paddingVertical: 27,
+    paddingHorizontal: 16,
   },
 });
 
