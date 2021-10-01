8bb0ae7b86c3214a398cd62ada7681b3c861b148
Oscar Alejandro López Ramos <oscar@alluxi.com>
2021-10-01 06:11:52 -0500

    Adoption request hook corrections

--- src/myapp/hooks/adoption/useAdoption.ts
+++ src/myapp/hooks/adoption/useAdoption.ts
@@ -1,15 +1,13 @@
 import api from '../../services/app-services';
-import {useNavigation} from '@react-navigation/native';
 import {useMutation, useQueryClient} from 'react-query';
 
 const AddAdoption = (data) => api.post('api/v1/adoption-request/', data, true);
 
 const useAdoption = () => {
-  const navigation = useNavigation();
   const queryClient = useQueryClient();
   return useMutation((data: any) => AddAdoption(data), {
     onSuccess: () => {
-      // Save Pet image.
+      // Save adoption.
       queryClient.invalidateQueries('request-adoptionAll');
       queryClient.invalidateQueries('adoption-request');
     },
