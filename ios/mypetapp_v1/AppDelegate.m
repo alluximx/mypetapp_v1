f3221ede1f145e1f5ebe2128b333f2c4aa2adead
Oscar Alejandro López Ramos <oscar@alluxi.com>
2021-10-07 10:23:33 -0500

    Maps api key config

--- ios/mypetapp_v1/AppDelegate.m
+++ ios/mypetapp_v1/AppDelegate.m
@@ -3,14 +3,14 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-
+#import <GoogleMaps/GoogleMaps.h>
 
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-
+  [GMSServices provideAPIKey:@"AIzaSyDR74KoGq38MoZi9Ae1Rbzx79jf9PVwWwM"]; // add this line using the api key obtained from Google Console
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"mypetapp_v1"
