a0944ffb75122bafddf29c7774282d35b8c3e7f8
Oscar Alejandro López Ramos <oscar@alluxi.com>
2021-10-20 07:58:32 -0500

    Fixed dependant query issue in home screen

--- src/myapp/hooks/user/useMyPets.ts
+++ src/myapp/hooks/user/useMyPets.ts
@@ -1,10 +1,13 @@
 import {useQuery} from 'react-query';
 import api from '../../services/app-services';
 
-const useMyPets = (userId: number) => {
-  return useQuery(['my-pets', userId], () =>
-    api.get('api/v1/pets/?owner_user=' + userId, true),
+const useMyPets = (userId: number) =>
+  useQuery(
+    ['my-pets', userId],
+    () => api.get('api/v1/pets/?owner_user=' + userId, true),
+    {
+      enabled: userId !== undefined,
+    },
   );
-};
 
 export default useMyPets;
