cee74e2e5a43b6665b5164077b577054a7f98d29
Oscar Alejandro López Ramos <oscar@alluxi.com>
2021-12-08 12:17:57 -0600

    Add button and format changes to money

--- src/myapp/layouts/payment-method/index.tsx
+++ src/myapp/layouts/payment-method/index.tsx
@@ -1,7 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useLayoutEffect} from 'react';
 import {Image, Platform, StyleSheet} from 'react-native';
 import {List} from '@ui-kitten/components';
 // My components
+import AddButton from '../../components/buttons/add-button';
 import DefaultLayout from '../../components/layouts/default-layout';
 import TitleHeader from '../../components/texts/title-header';
 import DefaultText from '../../components/texts/default-text';
@@ -21,6 +22,23 @@ export default ({navigation, route}): React.ReactElement => {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const paymentQuery = useGetPaymentMethod();
   const deleteQuery = useDeletePaymentMethod();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <AddButton
+          style={{backgroundColor: globalColors.backgroundDefault}}
+          iconStyle={{
+            tintColor: globalColors.greenSecondary,
+            height: 35,
+            width: 35,
+          }}
+          onAdd={() => navigation.navigate('AddPaymentMethod')}
+          isSubmit
+        />
+      ),
+    });
+  }, [navigation]);
 
   useEffect(() => {
     if (paymentQuery.data) {
