1314d44022cf02a51b04ba195b36e6596365cbb1
Oscar Alejandro López Ramos <oscar@alluxi.com>
2022-02-10 17:56:33 -0600

    refactor(appointments): rename delete appointment method

--- src/myapp/hooks/vets/useDeleteVetAppointment.ts
+++ src/myapp/hooks/vets/useDeleteVetAppointment.ts
@@ -2,17 +2,20 @@ import {useMutation, useQueryClient} from 'react-query';
 // Services.
 import api from '../../services/app-services';
 
-const deletePet = (appointmentId) =>
+const deleteAppointment = (appointmentId) =>
   api.delete(`api/v1/vets-appointments/`, appointmentId);
 
 const useDeleteAppointment = () => {
   const queryClient = useQueryClient();
 
-  return useMutation((appointmentId: string) => deletePet(appointmentId), {
-    onSuccess: () => {
-      queryClient.invalidateQueries('my-appointments');
+  return useMutation(
+    (appointmentId: string) => deleteAppointment(appointmentId),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('my-appointments');
+      },
     },
-  });
+  );
 };
 
 export default useDeleteAppointment;
