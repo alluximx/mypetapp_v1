45564d7bcb971bf18e7172b51f8257cc09d0f2c0
Oscar Alejandro López Ramos <oscar@alluxi.com>
2022-02-16 14:36:15 -0600

    refactor(payment-methods): remove unnecessary return keyword

--- src/myapp/hooks/payment-method/useGetPaymentMethods.ts
+++ src/myapp/hooks/payment-method/useGetPaymentMethods.ts
@@ -1,8 +1,7 @@
 import {useQuery} from 'react-query';
 import api from '../../services/app-services';
 
-const useGetPaymentMethods = () => {
-  return useQuery('user-cards', () => api.get('api/v1/user-list-cards/', true));
-};
+const useGetPaymentMethods = () =>
+  useQuery('user-cards', () => api.get('api/v1/user-list-cards/', true));
 
 export default useGetPaymentMethods;
