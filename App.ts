cee79c97db0923b1c98324d3c077fdb2b3bea5c3
Oscar Alejandro López Ramos <oscar@alluxi.com>
2022-03-09 07:23:14 -0600

    feat(sentry): Wrap the app with sentry

--- App.ts
+++ App.ts
@@ -1,3 +1,9 @@
 import App from './src/app/app.component';
+import * as Sentry from '@sentry/react-native';
 
-export default App;
+Sentry.init({
+  dsn:
+    'https://6a879586bd10429e9460792b135da179@o527363.ingest.sentry.io/6247263',
+});
+
+export default Sentry.wrap(App);
