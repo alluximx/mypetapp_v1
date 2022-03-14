5cc16d196304989d901193e246a0e18c13b8f384
Oscar Alejandro López Ramos <oscar@alluxi.com>
2022-03-14 12:19:36 -0600

    feat(firebase): send token to backend

--- src/myapp/services/fcm-service.ts
+++ src/myapp/services/fcm-service.ts
@@ -1,5 +1,11 @@
-import messaging from '@react-native-firebase/messaging';
+import {Platform} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import auth_service from './auth-service';
+import axios from 'axios';
+import environments from '../environments';
+import messaging from '@react-native-firebase/messaging';
+
+const api_url = environments.API_URL;
 
 class FCMService {
   getToken = async () => {
@@ -10,6 +16,29 @@ class FCMService {
         await AsyncStorage.setItem('fcmToken', fcmToken);
       }
     }
+
+    const token = await AsyncStorage.getItem('auth_token');
+    // Store the token in the back
+    const userData = await auth_service.me();
+    const userEmail = userData?.data?.email;
+
+    await axios.post(
+      api_url + 'api/v1/devices/',
+      {
+        active: true,
+        device_id: userEmail,
+        name: userEmail,
+        registration_id: fcmToken,
+        type: Platform.OS,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Token ' + token,
+        },
+      },
+    );
+
     console.info('fcm-token', fcmToken);
 
     return fcmToken;
