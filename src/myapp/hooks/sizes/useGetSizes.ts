31987b8bd80ec1a5fc7ff5da5569374f54c74a49
santiagoquintero95 <106178573+santiagoquintero95@users.noreply.github.com>
2022-05-26 19:08:08 -0300

    created size filter

--- src/myapp/hooks/sizes/useGetSizes.ts
+++ src/myapp/hooks/sizes/useGetSizes.ts
@@ -3,5 +3,5 @@ import api from '../../services/app-services';
 
 const useGetSizes = () =>
   useQuery('sizes', () => api.get('api/v1/sizes/is_active=true', true));
-  
+
 export default useGetSizes;
